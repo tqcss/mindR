b87e9dc0728424e6d8007f598d57ddde0b31f0f8
tqcss <134121905+tqcss@users.noreply.github.com>
2024-12-06 06:13:11 +0800

    initial commit

---
+++ popup.js
@@ -0,0 +1,66 @@
+// Constants
+const storageKey = ['mindrData']
+
+// Screens
+const mainScreen = document.getElementById('main')
+const manageScreen = document.getElementById('manage')
+const linksScreen = document.getElementById('links')
+
+// Popup Elements
+const manageButton = document.getElementById('manageButton')
+const linksButton = document.getElementById('linksButton')
+const placeholder = document.getElementById('placeholder')
+const reminderContainer = document.querySelector('.content')
+
+let userData = chrome.storage.local.get(storageKey, retrieveUserData)
+
+
+// Handle dynamic data rendering
+if (userData) {
+    // pass
+} else {
+    placeholder.style.display = 'block'
+}
+
+// Handle screen switching
+manageButton.addEventListener('click', () => (switchScreen(mainScreen, manageButton)))
+linksButton.addEventListener('click', () => (switchScreen(mainScreen, linksScreen)))
+
+
+// Helper functions
+function switchScreen(from, to) {
+    from.style.display = 'none'
+    to.style.display = 'block'
+}
+
+function renderUserData() {
+    // pass
+}
+
+function addUserData(timestamp, label) {
+    let reminderCard = document.createElement('li')
+    let reminderTimestamp = document.createElement('div')
+    let reminderLabel = document.createElement('div')
+
+    reminderTimestamp.textContent = timestamp
+    reminderTimestamp.classList.add('timestamp')
+
+    reminderLabel.textContent = label
+    reminderLabel.classList.add('reminder')
+
+    reminderContainer.appendChild(reminderCard)
+    reminderCard.appendChild(reminderTimestamp)
+    reminderCard.appendChild(reminderLabel)
+}
+
+function retrieveUserData(data) {
+    return data
+}
+
+function saveUserData() {
+    chrome.storage.local.set(storageKey, userData)
+}
+
+function addUserData() {
+
+}
