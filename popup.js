74dc1dc28c9bf8b0927b864506081a5bca59263f
tqcss <134121905+tqcss@users.noreply.github.com>
2024-12-16 02:35:14 +0800

    revamped ui

--- popup.js
+++ popup.js
@@ -1,12 +1,32 @@
 // Elements
-const exitButton = document.querySelector('.exit-button');
+const backButton = document.getElementById('back-button');
+const exitButton = document.getElementById('exit-button');
 const panels = document.getElementsByClassName('panel');
+const mainPanel = document.getElementById('main-panel');
+const managePanel = document.getElementById('manage-panel');
+const bookmarkPanel = document.getElementById('bookmark-panel');
+
+
+// Dynamic Variables
+let currentPanel = mainPanel;
+
+
+// Main function
+main();
+
+function main() {
+    currentPanel.style.display = 'block';
+    updateHeader();
+}
 
 
 // Handle Events
-exitButton.addEventListener('click', closePopup);
+exitButton.addEventListener('click', () => window.close());
 
 
-function closePopup() {
-    window.close();
+// Helper functions
+function updateHeader() {
+    if (currentPanel != mainPanel) {
+        backButton.style.display = 'block';
+    }
 }
